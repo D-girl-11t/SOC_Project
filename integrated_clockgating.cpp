dc3f736ca6f3317cf860a48d4bb11341fb5d8414
Jboyrox <45749588+Jboyrox@users.noreply.github.com>
2024-04-18 20:03:41 -0400

    Update integrated_clockgating.cpp

--- integrated_clockgating.cpp
+++ integrated_clockgating.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <string>
 #include<fstream>
+#include<map>
+#include<sstream>
 using namespace std;
 class integrated_clock_gating   // This Parse object will parse throgh the netlist and collect all the dflip flops , multiplexers and and gates in lists.
 {
@@ -9,8 +11,11 @@ class integrated_clock_gating   // This Parse object will parse throgh the netli
     vector<string>  dff;
     vector<string>  mux;
     vector<string>  andGate;
+    vector<string> inverter;
     vector<int> dff_indices;
     vector<int> mux_indices;
+    vector<int> and_indices;
+    vector<int> inv_indices;
    void  parse(const std::string lines[], int size)
     {
         int i;
@@ -21,6 +26,7 @@ class integrated_clock_gating   // This Parse object will parse throgh the netli
             int founddff =lines[i].find("DFFE");
             int foundmux =lines[i].find("mux2");
             int foundand =lines[i].find("and2");
+            int foundinverter = lines[i].find("inv");
              if(founddff != string::npos)
              {
                 dff_indices.push_back(i);
@@ -44,7 +50,7 @@ class integrated_clock_gating   // This Parse object will parse throgh the netli
              }
              else if (foundand != string::npos)
              {
-                cout<<"blahand";
+                and_indices.push_back(i);
                 string and_gate=lines[i];
                 int j;
                 for (j=1;j<=4;j++)
@@ -53,11 +59,66 @@ class integrated_clock_gating   // This Parse object will parse throgh the netli
                 andGate.push_back(and_gate);
                 i=i+j;
              }
+             else if (foundinverter != string::npos)
+             {
+                inv_indices.push_back(i);
+                string inv=lines[i];
+                int j;
+                for (j=1;j<=3;j++)
+                inv=inv +"\n" + lines[i+j];
+                
+                inverter.push_back(inv);
+                i=i+j;
+             }
+             
              else
              i++;
 
         }
     }
+//     void replace(const map<int, int>& myMap)
+//     {
+//         for (const auto& pair : myMap) {
+//         vector<string> muxcg ,dffcg ;
+//         string line;
+//         while (getline(mux[pair.second], line, '\n')) {
+//         muxcg.push_back(line);
+//     }
+//    while (getline(dff[pair.first], line, '\n')) {
+//         dffcg.push_back(line);
+//     }
+//     }
+
+//     }
+void replace (const map<int, pair<int,int> >& myMap)
+{
+     for (const auto& pair : myMap) {
+        // cout << "Key: " << pair.first << ", Value: (" << pair.second.first << ", " << pair.second.second << ")" << endl;
+        vector<string>muxcg,dffcg;
+        int casecg;
+        string line;
+        istringstream issdff(dff[pair.first]);
+        while (getline(issdff, line, '\n')) {
+        dffcg.push_back(line);
+    }
+    istringstream issmux(mux[pair.second.first]);
+        while (getline(issmux, line, '\n')) {
+        muxcg.push_back(line);
+    }
+    casecg = pair.second.second;
+    if (casecg)
+    {
+    size_t start_pos = muxcg[1].find_first_of('(');
+    size_t end_pos = muxcg[1].find_first_of(')', start_pos);
+    string dff_in = muxcg.substr(start_pos + 1, end_pos - start_pos - 1);
+    
+    }
+    else
+    {
+
+    }
+    }
+}
 
     void display()
     {
