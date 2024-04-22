50848309596d946a2a0bd48328069d760ee64128
D-girl-11t <dhanareddy11t@gmail.com>
2024-04-22 14:36:19 -0400

    commit

--- 1_synth.v
+++ 1_synth.v
@@ -1,4 +1,5 @@
 module flip(clk, en, d, q);
+wire cg0;
   wire 0;
   input clk;
   wire clk;
@@ -8,15 +9,18 @@ module flip(clk, en, d, q);
   wire en;
   output q;
   wire q;
-  sky130_fd_sc_hd_mux2_4 _1 (
-    .A0(q),
-    .A1(d),
-    .S(en),
-    .X(0)
-  );
-  sky130_fd_sc_hd_dfxtp_1 \q$_DFFE_PP  (
-    .CLK(clk),
-    .D(0),
-    .Q(q)
-  );
+sky130_fd_sc_hd__inv_1 0 (
+.A(en),
+.Y(cg0),
+);
+sky_130_fd_sc_hd_and21 0 (
+.A(clk),
+.B(cg0),
+.X(0)
+);
+sky130_fd_sc_hd_dfxtp_1 \q$_DFFE_PP  (
+.CLK(0),
+.D(q),
+.Q(q)
+);
 endmodule
--- integrated_clockgating.cpp
+++ integrated_clockgating.cpp
@@ -6,7 +6,9 @@
 #include<sstream>
 #include<algorithm>
 #include <cctype> 
-using namespace std;
+#include <regex>
+using namespace std;\
+
 class integrated_clock_gating   // This Parse object will parse throgh the netlist and collect all the dflip flops , multiplexers and and gates in lists.
 {
     public:
@@ -20,6 +22,8 @@ class integrated_clock_gating   // This Parse object will parse throgh the netli
     vector<int> inv_indices;
     vector<string> cg_wires;
     map<int, std::pair<int, int> > validation_output;
+    int casenumber = -1;
+    double ratio; 
 
    void  parse(const std::string lines[], int size)
     {
@@ -81,20 +85,96 @@ class integrated_clock_gating   // This Parse object will parse throgh the netli
 
         }
     }
-//     void replace(const map<int, int>& myMap)
-//     {
-//         for (const auto& pair : myMap) {
-//         vector<string> muxcg ,dffcg ;
-//         string line;
-//         while (getline(mux[pair.second], line, '\n')) {
-//         muxcg.push_back(line);
-//     }
-//    while (getline(dff[pair.first], line, '\n')) {
-//         dffcg.push_back(line);
-//     }
-//     }
 
-//     }
+    void validation ()
+        {
+            // Regular expressions to extract indices
+            std::regex dff_regex(R"(\.D\((.*?)\))");
+            std::regex dffout_regex(R"(\.Q\((.*?)\))");
+
+            for (int i = 0; i < dff.size(); i++) {
+            std::smatch match;
+            // std::cout<<"dff";
+            if (regex_search(dff[i], match, dff_regex)) {
+                std::string dff_output = match[1];
+                if (std::regex_search(dff[i], match, dff_regex)) {
+                    std::string dff_input = match[1];
+                // if (!output.empty()) {
+                    // output.pop_back();
+                    // output.pop_back();// Remove the last character
+                    // std::cout << "Lineoutput " << i << ": " << dff_output <<std::endl;
+                    // std::cout << "Lineinput " << i << ": " << dff_input <<std::endl;
+                    // } 
+                    // Iterate through MUXes
+                    for (int j = 0; j < mux.size(); j++) {
+
+                        // convert to single line
+                        std::string single_line_text;
+                        for (char c : mux[j]) {
+                            if (c != '\n') {
+                                single_line_text += c;
+                            }
+                        }
+                        std::vector<std::string> C_all;
+                        std::smatch mux_match;
+                        // extracting inputs irrespective of anything
+                                            
+                        
+                        
+                    // B = Remove everything before the first dot (.)
+                        std::regex regex_B(R"(\.(.*))");
+                        std::smatch match_B;
+                        if(std::regex_search(single_line_text, match_B, regex_B)){
+                            std::string B = match_B[1];
+                            // std::cout << "B = " << B << std::endl;
+                
+                    // C = Extract text within parenthesis
+                    //dont touch this code, its working
+                            std::regex regex_C(R"(\((.*?)\))");
+                            std::smatch match_C;
+                            auto B_begin = std::sregex_iterator(B.begin(), B.end(), regex_C);
+                            auto B_end = std::sregex_iterator();
+                            for (std::sregex_iterator it = B_begin; it != B_end; ++it) {
+                                std::smatch match = *it;
+                                if (match.size() > 1) {
+                                    std::string C = match[1];
+                                    C_all.push_back(C);
+                                }
+                            }
+                            // if (!C_all.empty()) {
+                            //     // std::cout << "elements of mux ";
+                            //     for (const auto& c : C_all) {
+                            //         std::cout << c << " ";
+                            //     }
+                            //     std::cout << std::endl;
+                            // } //else {std::cout << "no elements ? recheck" << std::endl;}
+                        }
+                    
+                    
+                        bool found_q = false;
+                        for (size_t k = 0; k < C_all.size(); ++k) {
+                            if (C_all[k] == dff_output && C_all.back()== dff_input) {
+                                casenumber = k;
+                                found_q = true;
+                                break;
+                            }
+                        }
+                        
+                        if (found_q) {
+                            // std::cout << "Found that unique mux" << std::endl;
+                            //update the dff, mux and case
+                            validation_output.insert({i, {j, casenumber}});
+                        } //else {std::cout << "Did not find 'q' in C_all" << std::endl;}
+                        
+                    // else {std::cout << "No selectors found ? Recheck" << std::endl;}
+                }
+                }
+            } //else {std::cout << "Line " << i << " dff does not contain '.Q('" << std::endl;}
+        }
+        ratio = (validation_output.size() / static_cast<double>(dff.size()) )*100;
+        std::cout<<"Ratio of DFF clock gated = "<< ratio << "%" << endl;
+        }
+
 
 
 void replace ()
@@ -372,19 +452,20 @@ int main(int argc, char* argv[]) {
     integrated_clock_gating p1;
     p1.parse(lines,numLines);
    //After parsing
-   cout<<"Parsing Completed"<<endl;
-     map<int, pair<int, int> > myMap;
-
-    // Add an element to the map
-    myMap[0] = make_pair(0, 1);
+    cout<<"Parsing Completed"<<endl;
+    p1.validation();
+    for (const auto& pair : p1.validation_output) {
+        std::cout << "Key: " << pair.first << ", Value: (" << pair.second.first << ", " << pair.second.second << ")" << std::endl;
+    }
+    cout<<"validation completed"<<endl;
 //    p1.display();
-   p1.replace();
+    p1.replace();
 
 //    p1.display();
-string s1;
-   s1=p1.modify_netlist(lines,numLines);
+    string s1;
+    s1=p1.modify_netlist(lines,numLines);
   
-   std::ofstream outFile(argv[1]); // Change the output filename if needed
+    std::ofstream outFile(argv[1]); // Change the output filename if needed
     if (outFile.is_open()) {
         outFile << s1; // Write the string to the file
         outFile.close();}
